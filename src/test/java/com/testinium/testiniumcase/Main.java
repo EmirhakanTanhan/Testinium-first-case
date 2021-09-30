ff6ffa4862c719d1875e4eee26e1ee46e0828aa4
Emirhakan Tanhan <emirhakan.tanhan@gmail.com>
2021-09-30 18:37:49 +0300

    final? I guess

--- src/test/java/com/testinium/testiniumcase/Main.java
+++ src/test/java/com/testinium/testiniumcase/Main.java
@@ -14,12 +14,26 @@ public class Main {
     @FindBy(css = "[data-cy='search-find-button']")
     public WebElement searchButton;
 
+    @FindBy(css = "[class='sc-1i52yn4-0 cXPYPb']")
+    public WebElement emptyPageIdentifier;
+
     @FindBy(xpath = "//li[@data-testid='pagination-list-item']/a[@role]/span[contains(text(), '2')]/..")
     public WebElement paginationButton;
 
-//    @FindBy(css = "[class='sc-1nx8ums-0 dyekHG']")
     @FindBy(xpath = "//li[@class='sc-1nx8ums-0 dyekHG']")
     public List<WebElement> products;
+
+    @FindBy(css = "[class='sc-84am1q-0 sc-1r48nyr-0 WZTpV kEgEYI']")
+    public WebElement cartButton;
+
+    @FindBy(xpath = "//div[@class='gg-w-offset-1 gg-w-6 gg-d-offset-1 gg-d-6 gg-t-offset-0 gg-t-7 gg-m-offset-0 gg-m-17 pull-right-m pr0']/div/div[@class='total-price']")
+    public WebElement productInBasket;
+
+    @FindBy(xpath = "//div[@class='gg-input gg-input-select ']/select")
+    public WebElement cartProductQuantity;
+
+    @FindBy(xpath = "//div[@class='gg-d-24 hidden-m update-buttons-container']/div/a[@class='btn-delete btn-update-item']")
+    public WebElement cartProductDeleteButton;
 
     public Main(WebDriver driver) {
         PageFactory.initElements(driver, this);
--- src/test/java/com/testinium/testiniumcase/MainTest.java
+++ src/test/java/com/testinium/testiniumcase/MainTest.java
@@ -9,11 +9,13 @@ import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.interactions.Actions;
 
+import java.sql.Time;
 import java.time.Duration;
 import java.util.Random;
 import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.Select;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class MainTest {
@@ -32,7 +34,7 @@ public class MainTest {
         main = new Main(driver);
     }
 
-    //@AfterEach
+    @AfterEach
     public void tearDown() {
         driver.quit();
     }
@@ -42,6 +44,10 @@ public class MainTest {
         //Type 'bilgisayar' and click to submit button, search page should come
         main.searchInput.sendKeys("bilgisayar");
         main.searchButton.click();
+
+        if (!driver.findElements(By.cssSelector("[class='sc-1i52yn4-0 cXPYPb']")).isEmpty()) {
+            driver.get(driver.getCurrentUrl());
+        }
 
         //Scroll down until getting the pagination button in the center of the view, then click the button
         JavascriptExecutor js = ((JavascriptExecutor) driver);
@@ -53,11 +59,15 @@ public class MainTest {
     }
 
     @Test
-    public void getProduct() {
+    public void compareProductPrices() {
         //Type 'bilgisayar' and click to submit button, search page should come
         main.searchInput.sendKeys("bilgisayar");
         driver.manage().timeouts().implicitlyWait(4, TimeUnit.SECONDS);
         main.searchButton.click();
+
+        if (!driver.findElements(By.cssSelector("[class='sc-1i52yn4-0 cXPYPb']")).isEmpty()) {
+            driver.get(driver.getCurrentUrl());
+        }
 
         //Scroll down until getting the pagination button in the center of the view, then click the button
         JavascriptExecutor js = ((JavascriptExecutor) driver);
@@ -65,18 +75,136 @@ public class MainTest {
         driver.manage().timeouts().implicitlyWait(4, TimeUnit.SECONDS);
         main.paginationButton.click();
 
+        if (!driver.findElements(By.cssSelector("[class='sc-1i52yn4-0 cXPYPb']")).isEmpty()) {
+            driver.get(driver.getCurrentUrl());
+        }
+
         //Get random product from the list
         Random rand = new Random();
         int upperBound = main.products.size();
         WebElement selectedProduct = main.products.get(rand.nextInt(upperBound));
 
+        //Hover over the product to be able to see 'add to cart' button
         Actions actions = new Actions(driver);
         actions.moveToElement(selectedProduct).perform();
 
+        //Save the price of the selected product
+        String priceTagOnList = selectedProduct.findElement(By.cssSelector("[class='sc-1tdlrg-0 yf6n83-0 jPaJyh elANpG sc-1n2i5kn-3 lcRLro']")).getText();
+        System.out.println(priceTagOnList);
+
+        //Scroll into view the 'add to cart' button and click it
         WebElement addToCartButton = selectedProduct.findElement(By.cssSelector("[class='qjixn8-0 sc-1bydi5r-0 dGNqQc pXiHf sc-1n49x8z-3 bhXnM']"));
         js.executeScript("arguments[0].scrollIntoView(true); return;", addToCartButton);
         driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
         addToCartButton.click();
+        WebDriverWait wt = new WebDriverWait(driver,5);
+        wt.until(ExpectedConditions.
+                invisibilityOfElementLocated(By.cssSelector("[class='gekhq4-7 MOSbm']")));
+
+        //Go into the cart page and compare prices
+        main.cartButton.click();
+        driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+        String priceTagOnCart = main.productInBasket.getText();
+        System.out.println(priceTagOnCart);
+
+        assertEquals(priceTagOnList, priceTagOnCart);
+    }
+
+    @Test
+    public void cartOperationChangeQuantity () {
+        //Type 'bilgisayar' and click to submit button, search page should come
+        main.searchInput.sendKeys("bilgisayar");
+        driver.manage().timeouts().implicitlyWait(4, TimeUnit.SECONDS);
+        main.searchButton.click();
+
+        if (!driver.findElements(By.cssSelector("[class='sc-1i52yn4-0 cXPYPb']")).isEmpty()) {
+            driver.get(driver.getCurrentUrl());
+        }
+
+        //Scroll down until getting the pagination button in the center of the view, then click the button
+        JavascriptExecutor js = ((JavascriptExecutor) driver);
+        js.executeScript("arguments[0].scrollIntoView(true); return;", main.paginationButton);
+        driver.manage().timeouts().implicitlyWait(4, TimeUnit.SECONDS);
+        main.paginationButton.click();
+
+        if (!driver.findElements(By.cssSelector("[class='sc-1i52yn4-0 cXPYPb']")).isEmpty()) {
+            driver.get(driver.getCurrentUrl());
+        }
+
+        //Get random product from the list
+        Random rand = new Random();
+        int upperBound = main.products.size();
+        WebElement selectedProduct = main.products.get(rand.nextInt(upperBound));
+
+        //Hover over the product to be able to see 'add to cart' button
+        Actions actions = new Actions(driver);
+        actions.moveToElement(selectedProduct).perform();
+
+        //Scroll into view the 'add to cart' button and click it
+        WebElement addToCartButton = selectedProduct.findElement(By.cssSelector("[class='qjixn8-0 sc-1bydi5r-0 dGNqQc pXiHf sc-1n49x8z-3 bhXnM']"));
+        js.executeScript("arguments[0].scrollIntoView(true); return;", addToCartButton);
+        driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+        addToCartButton.click();
+        WebDriverWait wt = new WebDriverWait(driver,5);
+        wt.until(ExpectedConditions.
+                invisibilityOfElementLocated(By.cssSelector("[class='gekhq4-7 MOSbm']")));
+
+        //Go into the cart page and change quantity of the product
+        main.cartButton.click();
+        driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+        Select dropdown = new Select(main.cartProductQuantity);
+        dropdown.selectByVisibleText("2");
+
+        //Verify the quantity is equal to 2
+        String productQuantity = main.cartProductQuantity.getAttribute("value");
+        assertEquals("2", productQuantity);
+    }
+
+    @Test
+    public void cartOperationDeleteProduct () {
+        //Type 'bilgisayar' and click to submit button, search page should come
+        main.searchInput.sendKeys("bilgisayar");
+        driver.manage().timeouts().implicitlyWait(4, TimeUnit.SECONDS);
+        main.searchButton.click();
+
+        if (!driver.findElements(By.cssSelector("[class='sc-1i52yn4-0 cXPYPb']")).isEmpty()) {
+            driver.get(driver.getCurrentUrl());
+        }
+
+        //Scroll down until getting the pagination button in the center of the view, then click the button
+        JavascriptExecutor js = ((JavascriptExecutor) driver);
+        js.executeScript("arguments[0].scrollIntoView(true); return;", main.paginationButton);
+        driver.manage().timeouts().implicitlyWait(4, TimeUnit.SECONDS);
+        main.paginationButton.click();
+
+        if (!driver.findElements(By.cssSelector("[class='sc-1i52yn4-0 cXPYPb']")).isEmpty()) {
+            driver.get(driver.getCurrentUrl());
+        }
+
+        //Get random product from the list
+        Random rand = new Random();
+        int upperBound = main.products.size();
+        WebElement selectedProduct = main.products.get(rand.nextInt(upperBound));
+
+        //Hover over the product to be able to see 'add to cart' button
+        Actions actions = new Actions(driver);
+        actions.moveToElement(selectedProduct).perform();
+
+        //Scroll into view the 'add to cart' button and click it
+        WebElement addToCartButton = selectedProduct.findElement(By.cssSelector("[class='qjixn8-0 sc-1bydi5r-0 dGNqQc pXiHf sc-1n49x8z-3 bhXnM']"));
+        js.executeScript("arguments[0].scrollIntoView(true); return;", addToCartButton);
+        driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+        addToCartButton.click();
+        WebDriverWait wt = new WebDriverWait(driver,10);
+        wt.until(ExpectedConditions.
+                invisibilityOfElementLocated(By.cssSelector("[class='gekhq4-7 MOSbm']")));
+
+        //Go into the cart page and delete product
+        main.cartButton.click();
+        driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+        main.cartProductDeleteButton.click();
+        wt.until(ExpectedConditions.
+                invisibilityOfElementLocated(By.xpath("//div[not(contains(@class,'hidden')) and contains(@id,'empty-cart-container')]")));
     }
 
 }
